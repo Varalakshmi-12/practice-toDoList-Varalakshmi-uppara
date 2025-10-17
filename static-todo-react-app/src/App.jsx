6c3369878031bfff61b4fdb515524aa0819709bf
Varalakshmi12 <pandu.chetan27@gmail.com>
2025-10-17 14:12:33 -0500

    react static todolist is committed

---
+++ static-todo-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+//import { useState } from 'react'
+import Header from "./components/Header.jsx"
+import ToDoList from "./components/ToDoList.jsx"
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="app-container">
+        <Header/>
+        <ToDoList/>
+
+      </div>
+      </>
+        
+  )
+}
+
+export default App;
